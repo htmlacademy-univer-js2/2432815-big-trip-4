45d4add3cc19c0470ed1fb20e1002e836325ca7e
Semen-Zharkov <129843838+Semen-Zharkov@users.noreply.github.com>
2024-05-27 17:12:36 +0500

    Большие перемены (часть 1)

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { render } from './render.js';
+import TripPresenter from './presenter/board-presenter.js';
+import FilterView from './view/filter-view.js';
+import PointsModel from './model/points-model.js';
+import { generateFilter } from './mock/filter.js';
+
+const siteMainElement = document.querySelector('.page-main');
+const siteHeaderElement = document.querySelector('.trip-main');
+
+const pointModel = new PointsModel();
+const tripPresenter = new TripPresenter(siteMainElement.querySelector('.trip-events'), pointModel);
+
+const filters = generateFilter(pointModel.point);
+
+render(new FilterView(filters), siteHeaderElement.querySelector('.trip-controls__filters'));
+
+tripPresenter.init();
