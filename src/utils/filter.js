72b1524ce41978a5a8340504932f605c0b3a5938
Semen-Zharkov <129843838+Semen-Zharkov@users.noreply.github.com>
2024-06-19 15:59:33 +0500

    Fix 2

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,10 +1,12 @@
-import { FilterType } from '../mock/constants';
-import { isPointFuture, isPointPast } from './point';
+import { FilterType } from '../mock/const';
+import { isFuturedPoint, isPresentedPoint, isPastedPoint } from './point';
 
 const filter = {
   [FilterType.EVERYTHING]: (points) => points,
-  [FilterType.FUTURE]: (points) => points.filter((point) => isPointFuture(point)),
-  [FilterType.PAST]: (points) => points.filter((point) => isPointPast(point))
+  [FilterType.FUTURE]: (points) => points.filter((point) => isFuturedPoint(point)),
+  [FilterType.PRESENT]: (points) => points.filter((point) => isPresentedPoint(point)),
+  [FilterType.PAST]: (points) => points.filter((point) => isPastedPoint(point)),
 };
 
 export { filter };
+
