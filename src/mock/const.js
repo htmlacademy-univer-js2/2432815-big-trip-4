d746e1bdecb09fcf9320761a86d4de8dd1f44942
Semen-Zharkov <129843838+Semen-Zharkov@users.noreply.github.com>
2024-06-19 16:03:38 +0500

    Fix 3

---
+++ src/mock/const.js
@@ -0,0 +1,104 @@
+import dayjs from 'dayjs';
+
+const DEFAULT_TYPE = 'flight';
+const DESTINATIONS_LENGTH_BORDER = 3;
+const AUTHORIZATION = 'Basic has76dgu2h83jdo';
+const END_POINT = 'https://21.objects.htmlacademy.pro/big-trip';
+
+const EVENT_TYPES = [
+  'Taxi',
+  'Bus',
+  'Train',
+  'Ship',
+  'Drive',
+  'Flight',
+  'Check-in',
+  'Sightseeing',
+  'Restaurant'
+];
+
+const EMPTY_POINT = {
+  type: DEFAULT_TYPE,
+  basePrice: 0,
+  dateFrom: dayjs().toDate(),
+  dateTo: dayjs().toDate(),
+  destination: null,
+  isFavorite: false,
+  offers: []
+};
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+  POST: 'POST',
+};
+
+const FilterType = {
+  EVERYTHING: 'everything',
+  FUTURE: 'future',
+  PRESENT: 'present',
+  PAST: 'past',
+};
+
+const Mode = {
+  DEFAULT: 'DEFAULT',
+  EDITING: 'EDITING',
+};
+
+const SortType = {
+  DAY: 'day',
+  EVENT: 'event',
+  TIME: 'time',
+  PRICE: 'price',
+  OFFERS: 'offers'
+};
+
+const UserAction = {
+  UPDATE_POINT: 'UPDATE_POINT',
+  ADD_POINT: 'ADD_POINT',
+  DELETE_POINT: 'DELETE_POINT',
+};
+
+const UpdateType = {
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+  INIT: 'INIT'
+};
+
+const ButtonText = {
+  CANCEL: 'Cancel',
+  DELETE: 'Delete',
+  DELETING: 'Deleting',
+  SAVE: 'Save',
+  SAVING: 'Saving',
+};
+
+const NoPointsTextType = {
+  NOPOINTS: 'Click New Event to create your first point',
+  LOADING: 'Loading...',
+
+};
+
+const TimeLimit = {
+  LOWER_LIMIT: 350,
+  UPPER_LIMIT: 1000
+};
+
+export {
+  EVENT_TYPES,
+  FilterType,
+  Mode,
+  SortType,
+  UserAction,
+  UpdateType,
+  NoPointsTextType,
+  ButtonText,
+  EMPTY_POINT,
+  Method,
+  TimeLimit,
+  DESTINATIONS_LENGTH_BORDER,
+  AUTHORIZATION,
+  END_POINT
+};
