55a1990d247813568934b1dfd5678333f77d72d4
Semen-Zharkov <129843838+Semen-Zharkov@users.noreply.github.com>
2024-06-19 15:48:21 +0500

    Fix 2

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,54 +1,46 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-const createFilterItemTemplate = (filter, currentFilterType) => {
-  const { type, name, count } = filter;
+function createFilterItemElement(filter, currentFilterType) {
+  const { type, count } = filter;
+  return `
+    <div class="trip-filters__filter">
+      <input id="filter-${type}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${type}" ${type === currentFilterType ? 'checked' : ''} ${count ? '' : 'disabled'}>
+      <label class="trip-filters__filter-label" for="filter-${type}">${type}</label>
+    </div>`;
+}
 
-  return (
-    `<div class="trip-filters__filter">
-    <input id="filter-${name}"
-    class="trip-filters__filter-input
-    visually-hidden" type="radio"
-    name="trip-filter"
-    value="${type}"
-    ${type === currentFilterType ? 'checked' : ''}
-    ${count === 0 ? 'disabled' : ''}>
-      <label class="trip-filters__filter-label" for="filter-${name}">${name}</label>
-    </div>`
-  );
-};
-
-const createFilterTemplate = (filterItems, currentFilterType) => {
-  const filterItemsTemplate = filterItems
-    .map((filter) => createFilterItemTemplate(filter, currentFilterType))
+function createFilterElement(filterItems, currentFilterType) {
+  const filterItemsTemplate = filterItems.map((filter) =>
+    createFilterItemElement(filter, currentFilterType))
     .join('');
 
-  return `<form class="trip-filters" action="#" method="get">
-    ${filterItemsTemplate}
-    <button class="visually-hidden" type="submit">Accept filter</button>
+  return `
+    <form class="trip-filters" action="#" method="get">
+      ${filterItemsTemplate}
+      <button class="visually-hidden" type="submit">Accept filter</button>
     </form>`;
-};
+}
 
 export default class FilterView extends AbstractView {
-  #filters = null;
+  #filters = [];
   #currentFilter = null;
+  #handleFilterTypeChange = null;
 
-  constructor(filters, currentFilterType) {
+  constructor({ filters, currentFilterType, onFilterTypeChange }) {
     super();
     this.#filters = filters;
     this.#currentFilter = currentFilterType;
+    this.#handleFilterTypeChange = onFilterTypeChange;
+
+    this.element.addEventListener('change', this.#filterTypeChangeHandler);
   }
 
   get template() {
-    return createFilterTemplate(this.#filters, this.#currentFilter);
+    return createFilterElement(this.#filters, this.#currentFilter);
   }
-
-  setFilterTypeChangeHandler = (callback) => {
-    this._callback.filterTypeChange = callback;
-    this.element.addEventListener('change', this.#filterTypeChangeHandler);
-  };
 
   #filterTypeChangeHandler = (evt) => {
     evt.preventDefault();
-    this._callback.filterTypeChange(evt.target.value);
+    this.#handleFilterTypeChange(evt.target.value);
   };
 }
