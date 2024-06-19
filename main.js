75f884637d8c0fafe3bc7b9d44a99f05ea05503f
Semen-Zharkov <129843838+Semen-Zharkov@users.noreply.github.com>
2024-06-19 15:41:36 +0500

    Fix 1

---
+++ main.js
@@ -0,0 +1,58 @@
+import PointModel from './model/point-model';
+import { RenderPosition, render } from './framework/render';
+import OfferModel from './model/offer-model';
+import FilterModel from './model/filter-model';
+import FilterPresenter from './presenter/filter-presenter';
+import TripPresenter from './presenter/board-presenter';
+import NewPointButtonView from './view/new-point-button-view';
+import PointsApiService from './points-api-service';
+import DestinationModel from './model/destination-model';
+import { AUTHORIZATION, END_POINT } from './mock/const';
+
+const mainTripElement = document.querySelector('.trip-main');
+const filterElement = mainTripElement.querySelector('.trip-controls__filters');
+const tripEvents = document.querySelector('.trip-events');
+const pointApiService = new PointsApiService(END_POINT, AUTHORIZATION);
+
+const offerModel = new OfferModel(pointApiService);
+const destinationModel = new DestinationModel(pointApiService);
+const filterModel = new FilterModel();
+const pointModel = new PointModel(pointApiService);
+
+const tripPresentor = new TripPresenter({
+  tripMainContainer: mainTripElement,
+  eventsContainer: tripEvents,
+  pointModel,
+  destinationModel,
+  offerModel,
+  filterModel,
+  onNewPointDestroy: handleNewPointFormClose
+});
+const filterPresenter = new FilterPresenter({
+  filterContainer: filterElement,
+  filterModel,
+  pointModel
+});
+
+const newPointButtonComponent = new NewPointButtonView({
+  onClick: handleNewPointButtonClick,
+});
+
+function handleNewPointFormClose() {
+  newPointButtonComponent.element.disabled = false;
+}
+
+function handleNewPointButtonClick() {
+  tripPresentor.createPoint();
+  newPointButtonComponent.element.disabled = true;
+}
+
+filterPresenter.init();
+tripPresentor.init();
+offerModel.init().finally(() => {
+  destinationModel.init().finally(() => {
+    pointModel.init().finally(() => {
+      render(newPointButtonComponent, mainTripElement, RenderPosition.BEFOREEND);
+    });
+  });
+});
