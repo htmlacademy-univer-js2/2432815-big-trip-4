7e8db628b8a4995b3a3a27fbbcf23c4b552f45a6
Semen-Zharkov <129843838+Semen-Zharkov@users.noreply.github.com>
2024-06-19 16:02:28 +0500

    Fix 2

--- src/model/filter-model.js
+++ src/model/filter-model.js
@@ -1,5 +1,5 @@
 import Observable from '../framework/observable.js';
-import { FilterType } from '../mock/constants.js';
+import { FilterType } from '../mock/const.js';
 
 export default class FilterModel extends Observable {
   #filter = FilterType.EVERYTHING;
@@ -8,8 +8,8 @@ export default class FilterModel extends Observable {
     return this.#filter;
   }
 
-  setFilter = (updateType, filter) => {
+  setFilter(updateType, filter) {
     this.#filter = filter;
     this._notify(updateType, filter);
-  };
+  }
 }
